d555c64a844d1e56d9369cdbe8185498f4a28137
王科[avenue] <avenue@canway.net>
2021-11-18 20:39:01 +0800

    fix周末提示

--- count_time.go
+++ count_time.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -34,26 +36,35 @@ func getDayDiffer(start_time, end_time string) int64 {
 }
 
 //打印信息
-func fishingReminder(time_year, time_spring string) {
-	/*传入周六，元旦，春节时间*/
+func fishingReminder(time_spring string) {
+	/*传入春节时间*/
 	time_now := time.Now().Format("2006-01-02 15:04:05")
 	hour := time.Now().Hour()
 	today := time.Now().Weekday()
 	day := time.Saturday - today
+	year := time.Now().Year() + 1
+	string_year := strconv.Itoa(year)
+	time_year := string_year + "-01-01 00:00:00"
+	time_spring = "2022-02-01 00:00:00"
 	fmt.Print("摸鱼办提醒您：\n")
-	fmt.Printf("%s%s，摸鱼人！\n", time_now, judgeMorningOrAfternoon(hour))
-	fmt.Println("工作再累 一定不要忘记摸鱼哦！")
-	fmt.Println("有事没事起身去茶水间去厕所去廊道走走")
-	fmt.Println("别老在工位上坐着钱是老板的,但命是自己的")
-	fmt.Printf("距离周末假期还有%d天\n", day)
-	fmt.Printf("距离元旦假期还有%d天\n", getDayDiffer(time_now, time_year))
-	fmt.Printf("距离春节假期还有%d天\n", getDayDiffer(time_now, time_spring))
-	fmt.Println("上班是帮老板赚钱，摸鱼是赚老板的钱！\n最后，祝愿天下所有摸鱼人，都能愉快的渡过每一天.")
+	if today == time.Sunday || today == time.Saturday {
+		fmt.Println("今天摸鱼办也休息哦")
+		fmt.Println("周末愉快")
+		time.Sleep(time.Second * 10)
+		os.Exit(1)
+	} else {
+		fmt.Printf("%s%s，摸鱼人！\n", time_now, judgeMorningOrAfternoon(hour))
+		fmt.Println("工作再累 一定不要忘记摸鱼哦！")
+		fmt.Println("有事没事起身去茶水间去厕所去廊道走走")
+		fmt.Println("别老在工位上坐着钱是老板的,但命是自己的")
+		fmt.Printf("距离周末假期还有%d天\n", day)
+		fmt.Printf("距离元旦假期还有%d天\n", getDayDiffer(time_now, time_year))
+		fmt.Printf("距离春节假期还有%d天\n", getDayDiffer(time_now, time_spring))
+		fmt.Println("上班是帮老板赚钱，摸鱼是赚老板的钱！\n最后，祝愿天下所有摸鱼人，都能愉快的渡过每一天.")
+	}
 }
 
 func main() {
-	time_year = "2022-01-01 00:00:00"
-	time_spring = "2022-02-01 00:00:00"
-	fishingReminder(time_year, time_spring)
+	fishingReminder(time_spring)
 	time.Sleep(time.Second * 30)
 }
